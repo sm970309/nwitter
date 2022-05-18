62306268a2856c1f21c1e9765129e6d209aa6802
sm970309 <sm970309@gmail.com>
2022-05-18 13:31:23 +0900

    config setting

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import firebase from "./firebase";
+console.log(firebase)
 function App() {
   return (
     <div className="App">
---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import {initializeApp} from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAmjl19GYUg0emRh0JH6KRfnbb8HIxiyDU",
+    authDomain: "nwitter-ff1a0.firebaseapp.com",
+    projectId: "nwitter-ff1a0",
+    storageBucket: "nwitter-ff1a0.appspot.com",
+    messagingSenderId: "964762026269",
+    appId: "1:964762026269:web:810ae211ef69d22e9421f4"
+};
+
+export default initializeApp(firebaseConfig);
