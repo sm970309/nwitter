a3d129f937caa711ac3a8c3cbb0866d2e0c45ba0
sm970309 <sm970309@gmail.com>
2022-05-19 17:10:30 +0900

    로그인 기능 구현

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,22 @@
-import {auth} from "myFireBase";
 import AppRouter from "components/Router";
 import React, { useState } from "react";
+import {initializeApp} from "firebase/app";
+import {getAuth} from "firebase/auth";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAmjl19GYUg0emRh0JH6KRfnbb8HIxiyDU",
+    authDomain: "nwitter-ff1a0.firebaseapp.com",
+    projectId: "nwitter-ff1a0",
+    storageBucket: "nwitter-ff1a0.appspot.com",
+    messagingSenderId: "964762026269",
+    appId: "1:964762026269:web:810ae211ef69d22e9421f4"    
+};
+export const fb = initializeApp(firebaseConfig);
+export const auth = getAuth();
+
 
 function App() {
-  console.log(auth)
   const [isLoggedIn, setIsLoggedIn] = useState(auth.currentUser);
   return (
     <div>
--- src/routers/Auth.js
+++ src/routers/Auth.js
@@ -1,7 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, getAuth } from "firebase/auth";
 const Auth = () => {
-    return(
-    <span>This is Auth page</span>
-)}
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [user, setUser] = useState(false)
+    const onChange = (event) => {
+        const { target: { name, value } } = event;
+        console.log(name)
+        if (name === "email") {
+            setEmail(value);
+        }
+        else {
+            setPassword(value);
+        }
+
+    }
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        const auth = getAuth();
+        let data;
+        try {
+            if (user) {
+                data = await signInWithEmailAndPassword(auth, email, password)
+                alert('로그인 성공')
+            }
+            else {
+                data = await createUserWithEmailAndPassword(auth, email, password)
+                alert("가입성공")
+                setUser(true);
+            }
+        }catch(error){
+            alert(error)
+        }
+
+    }
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input
+                    name="email"
+                    type="text"
+                    placeholder="Email"
+                    required
+                    value={email}
+                    onChange={onChange}
+                />
+                <input
+                    name="password"
+                    type="password"
+                    placeholder="Password"
+                    required
+                    value={password}
+                    onChange={onChange}
+                />
+                <input type="submit" value="Log In" />
+            </form>
+            <div>
+                <button>Continue with Google</button>
+                <button>Continue with Github</button>
+            </div>
+
+        </div>
+
+    )
+}
 
 export default Auth;
