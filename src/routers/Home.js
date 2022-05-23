08e975c85955e3565459f083fd7493790fa23be6
sm970309 <sm970309@gmail.com>
2022-05-23 15:34:13 +0900

    데이터베이스 저장 기능 collection

--- src/routers/Home.js
+++ src/routers/Home.js
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import { addDoc, collection, getFirestore } from "firebase/firestore";
+import { fb } from "components/App";
+
 const Home = () => {
+    const [nweet,setNweet] = useState('');
+    const onSubmit = async (event) =>{
+        event.preventDefault();
+        const db = getFirestore(fb)
+        try{
+            const docRef = await addDoc(collection(db,'nweet'),{
+                nweet,
+                createdAt:Date.now(),
+            })
+            console.log(docRef)
+        }
+        catch(e){
+            console.error("Error: ",e)
+        }
+        setNweet('')
+    }
+    const onChange =(event) =>{
+        setNweet(event.target.value)
+        
+    }
     return(
-    <span>This is Home page</span>
+    <div>
+        <form onSubmit={onSubmit}>
+            <input onChange={onChange} value={nweet} type="text" placeholder="What's on your mind?" maxLength={120}/>
+            <input type="submit" value="Nweet"/>
+        </form>
+    </div>
 )}
 
 export default Home;
