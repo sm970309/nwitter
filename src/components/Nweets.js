62de8327ab60953094eaa76afa65d36a31760368
sm970309 <sm970309@gmail.com>
2022-05-24 10:59:06 +0900

    수정기능 추가

--- src/components/Nweets.js
+++ src/components/Nweets.js
@@ -1,27 +1,55 @@
-import { deleteDoc, doc, getFirestore } from "firebase/firestore";
-import React from "react";
+import { deleteDoc, doc, getFirestore, updateDoc } from "firebase/firestore";
+import React, { useState } from "react";
 import { fb } from "./App";
 
 const Nweet = ({ nweetObj, isOwner }) => {
-    const onDelete = async() =>{
-        const db = getFirestore(fb)
-        const NweetTextRef = doc(db,'nweet',`${nweetObj.id}`)
+    const [editing,setEditing] = useState(false);
+    const [newNweet,setNewNweet] = useState(nweetObj.text);
+    const db = getFirestore(fb)
+    const NweetTextRef = doc(db,'nweet',`${nweetObj.id}`)
+    const onDelete = async() =>{   
         const ok = window.confirm("Are you Sure?")
         if (ok){
-            console.log(nweetObj.id)
             await deleteDoc(NweetTextRef);
         }
     }
+    const toggleEditing = () =>{
+        setEditing(prev => !prev)
+        setNewNweet(nweetObj.text)
+    }
+    const onChange = (event) =>{
+        setNewNweet(event.target.value)
+    }
+    const onEdit = async (event) =>{
+        event.preventDefault();
+        setEditing(prev => !prev)
+        updateDoc(NweetTextRef,{
+            text:newNweet
+        })
+    }
     return (
         <div>
+            {!editing? 
+            <>
             <h3>{nweetObj.text}</h3>
             <h5>{nweetObj.createName}</h5>
             {isOwner && 
             <>
                 <button onClick={onDelete}>Delete</button>
-                <button>Edit</button>
+                <button onClick={toggleEditing}>Edit</button>
             </>
             }
+            </>:
+            <>
+            <form  onSubmit={onEdit}>
+                <input value={newNweet} type="text" onChange={onChange} required/>
+                <input value="Update" type="submit"/>
+            </form>
+            <button onClick={toggleEditing}>Cancel</button>      
+            </>      
+            }
+            
+            
         </div>
     )
 }
