079eed5f24a2faf2a83ea394fef8fe03ba9793fe
sm970309 <sm970309@gmail.com>
2022-05-25 15:10:40 +0900

    style 적용(signup)

--- src/routers/Signup.js
+++ src/routers/Signup.js
@@ -1,12 +1,15 @@
 import { createUserWithEmailAndPassword, getAuth, updateProfile } from "firebase/auth"
 import { useState } from "react";
 import { Link } from "react-router-dom"
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Signup = () => {
     const auth = getAuth();
     const [email, setEmail] = useState("");
     const [name, setName] = useState("");
     const [password, setPassword] = useState("");
+    const [password2, setPassword2] = useState("");
     const onChange = (event) => {
 
         const { target: { name, value } } = event;
@@ -16,6 +19,9 @@ const Signup = () => {
         else if (name === "password") {
             setPassword(value);
         }
+        else if (name === "password2") {
+            setPassword2(value);
+        }
         else if (name === "name") {
             setName(value);
         }
@@ -23,11 +29,14 @@ const Signup = () => {
     }
     const onSubmit = async (event) => {
         event.preventDefault();
-        console.log(email, password, name)
+        if(password!==password2){
+            alert("Incorrect password confirmation")
+            return
+        }
         try {
             await createUserWithEmailAndPassword(auth, email, password)
-            updateProfile(auth.currentUser,{
-                displayName:name
+            updateProfile(auth.currentUser, {
+                displayName: name
             })
             alert('회원가입 성공')
 
@@ -36,46 +45,54 @@ const Signup = () => {
         }
     }
     return (
-        <div>
-            <form onSubmit={onSubmit}>
-                <div>
-                    <label>Email: </label>
-                    <input
-                        name="email"
-                        type="text"
-                        placeholder="Email"
-                        required
-                        value={email}
-                        onChange={onChange}
-                    />
-                </div>
-                <div>
-                    <label>Password: </label>
-                    <input
-                        name="password"
-                        type="password"
-                        placeholder="Password"
-                        required
-                        value={password}
-                        onChange={onChange}
-                    />
-
-                </div>
-                <div>
-                    <label>Name: </label>
-                    <input
-                        name="name"
-                        type="text"
-                        placeholder="Name"
-                        required
-                        value={name}
-                        onChange={onChange}
-                    />
-                </div>
-                <input value="Sign Up" type="submit" />
-                <button>
-                    <Link to="/">Go Home</Link>
-                </button>
+        <div className="authContainer">
+            <form onSubmit={onSubmit} className="container">
+                <FontAwesomeIcon
+                    icon={faTwitter}
+                    color={"#04AAFF"}
+                    size="3x"
+                    style={{ marginBottom: 30 }}
+                />
+                <input
+                    name="email"
+                    type="text"
+                    placeholder="Email"
+                    required
+                    value={email}
+                    onChange={onChange}
+                    className="authInput"
+                />
+                <input
+                    name="password"
+                    type="password"
+                    placeholder="Password"
+                    required
+                    value={password}
+                    onChange={onChange}
+                    className="authInput"
+                />
+                <input
+                    name="password2"
+                    type="password"
+                    placeholder="Password Again"
+                    required
+                    value={password2}
+                    onChange={onChange}
+                    className="authInput"
+                />
+                <input
+                    name="name"
+                    type="text"
+                    placeholder="Name"
+                    required
+                    value={name}
+                    onChange={onChange}
+                    className="authInput"
+                />
+                <input value="Sign Up" type="submit" className="authInput authSubmit" />
+                <center>
+                    <Link to="/" >Go Home</Link>
+                </center>
             </form>
 
         </div>
